f2ee0b6a383dd4eca2450587fd01295eb58017ec
Salman Ahamd <salman.ahamd@LandMarkGroup.India>
2025-02-11 18:10:07 +0530

    updated Python file for ports

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import sys
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/')
+def home():
+    return "Hello! Flask App Running Successfully."
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
